364c57d8ef318507185d17eaf2672d298f0d6dbf
aman1210 <aman.srivastava101@gmail.com>
2021-10-06 18:18:08 +0530

    adding brush size option

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kidsdrawingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kidsdrawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.drawingView.setSizeForBrush(20.toFloat())
     }
 }
