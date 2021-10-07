7b2240394b2b1b4e01b2ca89a3de9572aa880766
aman1210 <aman.srivastava101@gmail.com>
2021-10-07 11:10:50 +0530

    brush size change dialog added

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,16 +1,49 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kidsdrawingapp.databinding.ActivityMainBinding
+import com.example.kidsdrawingapp.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var brushBinding : DialogBrushSizeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.drawingView.setSizeForBrush(20.toFloat())
+
+        binding.ibBrush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
+    }
+
+    private fun showBrushSizeChooserDialog(){
+        brushBinding = DialogBrushSizeBinding.inflate(layoutInflater)
+        val brushDialog= Dialog(this)
+        brushDialog.setContentView(brushBinding.root)
+        brushDialog.setTitle("Brush Size: ")
+        val smallButton = brushBinding.ibSmallBrush
+        smallButton.setOnClickListener {
+            binding.drawingView.setSizeForBrush(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val mediumButton = brushBinding.ibMediumBrush
+        mediumButton.setOnClickListener {
+            binding.drawingView.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        val largeButton = brushBinding.ibLargeBrush
+        largeButton.setOnClickListener {
+            binding.drawingView.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
     }
 }
