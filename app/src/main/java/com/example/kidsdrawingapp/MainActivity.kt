dc78712d58a67f4deee586be4d451816b70ac77b
Aman Srivastava <aman.srivastava101@gmail.com>
2021-11-01 12:43:49 +0530

    sharing image

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -211,8 +212,9 @@ class MainActivity : AppCompatActivity() {
                             Toast.makeText(
                                 this@MainActivity,
                                 "File saved successfully $result",
-                                Toast.LENGTH_LONG
+                                Toast.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         }else{
                             Toast.makeText(
                                 this@MainActivity,
@@ -244,6 +246,17 @@ class MainActivity : AppCompatActivity() {
         if(customProgressDialog!=null){
             customProgressDialog?.dismiss()
             customProgressDialog=null
+        }
+    }
+
+    private fun shareImage(result:String){
+        MediaScannerConnection.scanFile(this, arrayOf(result),null){
+            path, uri ->
+            var shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM,uri)
+            shareIntent.type="image/png"
+            startActivity(Intent.createChooser(shareIntent,"Share"))
         }
     }
 
